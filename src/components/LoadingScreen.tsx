68768900d314d9f7acb899212f9fe28e0e021aba
Brandon Leroux <brandonlacoste9-tech@users.noreply.github.com>
2025-11-27 16:45:34 -0500

    Fix LoadingScreen import path for utils

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { LogoFull } from './Logo';
-import { cn } from '../../lib/utils';
+import { cn } from '../lib/utils';
 
 interface LoadingScreenProps {
   message?: string;
